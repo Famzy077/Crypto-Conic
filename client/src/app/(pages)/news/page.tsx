b610da6eee9c09df7a88d4c83fe46b686b24eb3d
Akinola Femi <akinolafemi573@gmail.com>
2025-07-29 20:35:20 +0100

    update UI

--- client/src/app/(pages)/news/page.tsx
+++ client/src/app/(pages)/news/page.tsx
@@ -8,7 +8,7 @@ export default async function NewsPage() {
 
     return (
         <main className="bg-gray-50 mt-18 max-sm:mt-14 dark:bg-gray-900 min-h-screen">
-            <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12">
+            <div className="container mx-auto px-4 sm:px-6 lg:px-8 pt-7">
                 <div className="text-center mb-12">
                     <h1 className="text-4xl md:text-5xl font-bold text-gray-800 dark:text-white">Crypto News</h1>
                     <p className="text-lg text-gray-500 dark:text-gray-400 mt-2">
